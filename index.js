fd85e158553790060b3b7d7595cf28f6a15f0979
Glitch (hello-express) <none>
2020-07-18 20:51:09 +0000

    🚖🎷 Checkpoint ./index.js:13134877/5431 ./config.js:13134877/22:17210519/64 ./package.json:13134877/1384

--- index.js
+++ index.js
@@ -1,20 +1,74 @@
-const Discord = require("discord.js"); //Exporting Package | If You Ever Get Error [MODULE_NOT_FOUND] Than You Need To Install A Package!
+const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
-const client = new Discord.Client(); //Creating A Client/Bot From Discord.js Package To Do Stuff!
-const { Prefix, Token, Owner, Color } = require("./config.js"); //Exporting File Things File Name Config.js And Getting Things Like : Prefix Token And Owner ID!
+const client = new Discord.Client();
+const { Prefix, Token, Owner, Color } = require("./config.js");
+const fs = require("fs");
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.descriptions = new Discord.Collection();
 
-//Ready Event | When Bot Is Ready Bot Send A Message To Console + Bot Things When Bot Get Online Example : Handle Activity!
+//Ready Event
 client.on("ready", async () => {
   console.log(`Ready To Go No Error So Far!`); //Console Message!
   client.user.setActivity(`watching all of yall :)`, { type: "PLAYING" }); //Bot Activity!
 });
 
+let modules = [
+  "Members"
+];
+
+modules.forEach(function(module) {
+  fs.readdir(`./commands/${module}`, function(err, files) {
+    if (err) throw err;
+    files.forEach(function(file) {
+      if (!file.endsWith(".js")) return;
+      let command = require(`./commands/${module}/${file}`);
+      console.log(`${command.name}.js - Successfully loaded!`);
+      if (command.name) client.commands.set(command.name, command);
+      if (command.description) {
+        client.descriptions.set(command.description, command.description);
+      }
+      if (command.aliases) {
+        command.aliases.forEach(alias => client.aliases.set(alias, command.name));
+      }
+    });
+  });
+});
+
 client.on("message", async message => {
   if (message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) {
-    return message.channel.send(new MessageEmbed()
-                               .setColor(`${Color}`))
+    return message.channel.send(
+      new MessageEmbed()
+        .setColor(`${Color}`)
+        .setTitle(`Information`)
+        .setDescription(`Bot prefix : ${Prefix} | Help command : ${Prefix}help`)
+        .setTimestamp()
+        .setFooter(`Requested by ${message.author.username}`)
+    );
   }
-})
+});
 
-//Bot Login! Finally!
+client.on("message", async message => {
+  if (message.channel.type === "dm") {
+    if (message.author.bot) return;
+
+
+  if (message.content.includes(client.user.id))
+    message.channel.send(
+      `Hello! Im Verziles and the prefix for this guild is: ${Prefix}`
+      )
+
+  if (message.content.indexOf(Prefix)) return;
+
+  let messageArray = message.content.split(` `);
+  let args = messageArray.slice(1);
+  let cmd = messageArray[0].toLowerCase();
+
+  let command =
+    client.commands.get(cmd.slice(Prefix.length)) ||
+    client.commands.get(client.aliases.get(cmd.slice(Prefix.length)));
+  if (command) command.run(client, message, args);
+
+}});
+
 client.login(Token); //Bot Login With Token!

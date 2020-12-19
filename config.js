509660ba7f2ff1bc3e5aa1b5c83e2950206a9043
Tomer <66119864+ProStem@users.noreply.github.com>
2020-12-19 20:09:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.Prefix = `-`; //Normal Default Prefix!
-exports.Token = `NzM0NDkyODc3Njg5OTc4OTUw.XxSgSA.O3ab_2LnZPSstP7THgGRWMZ6D5g`; //Bot Token!
+exports.Token = ``; //Bot Token!
 exports.Owner = `404665146137640970`; //Bot Owner ID!
 exports.Color = `BLUE`; //All Bot Message Embed Color!
